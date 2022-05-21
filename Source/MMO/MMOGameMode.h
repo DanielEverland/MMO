9badd3c16cd7a33c4f733b16226c942f888b8e53
Daniel Everland <danieleverland@gmail.com>
2022-05-21 22:33:02 +0200

    Initial commit

---
+++ Source/MMO/MMOGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MMOGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMMOGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMMOGameMode();
+};
+
+
+
